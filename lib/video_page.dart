ef5d0c108a2e3e1ffa4bb3b3e87a5df2e672f5a8
leejaemin <ljmhhh@gmail.com>
2021-07-08 19:50:54 +0900

    5

--- lib/video_page.dart
+++ lib/video_page.dart
@@ -2,12 +2,23 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:volume_controller/volume_controller.dart';
 import 'package:video_player/video_player.dart';
 import 'VideoProgressIndicator_ljm.dart';
 
 bool _hitTest = true;
 bool _hitTestHold = false;
 bool _fullscreen = false;
+Icon _playAndPauseIcon = Icon(Icons.play_arrow_rounded);
+Icon _volIcon = Icon(Icons.volume_up_rounded);
+
+double _getVolume = 0;
+double _muteVolume = 0;
+double _testVolume = 0;
+
+Timer _autoHideTimeout = Timer(Duration(milliseconds: 6000), (){
+  _hitTest = false;
+});
 
 class videoPage extends StatefulWidget {
   @override
@@ -17,10 +28,11 @@ class videoPage extends StatefulWidget {
 }
 
 class _videoPage extends State<videoPage>{
-  double _contextHeightPercent = 100;
   double _contextWidthPercent = 100;
+  double _contextHeightPercent = 100;
 
   VideoPlayerController _controller;
+  VolumeController _volController = new VolumeController();
 
   @override
   void initState() {
@@ -29,13 +41,48 @@ class _videoPage extends State<videoPage>{
     _hitTestHold = false;
     _fullscreen = false;
 
+    _volController.listener((volume) {
+      _hitTest = true;
+      autohide.auto();
+      _getVolume = volume;
+      print("Testset");
+      _controller.notifyListeners();
+    });
+
     _controller = VideoPlayerController.network('https://player.vimeo.com/external/491506029.hd.mp4?s=d6e509b47a07c2843b7f3cae4137bc8fafa748be&profile_id=174');
 
+    _controller.setLooping(false);
+    _controller.setVolume(1);
+
     _controller.addListener(() {
+      int _videoPosition = _controller.value.position.inMilliseconds;
+      int _videoDuration = _controller.value.duration.inMilliseconds;
+
       setState(() {
+        if(_videoDuration < 1){
+          _playAndPauseIcon = Icon(Icons.more_horiz_rounded);
+        }else if((_videoPosition < _videoDuration) && _controller.value.isPlaying){
+          _playAndPauseIcon = Icon(Icons.pause_rounded);
+        }else if((_videoPosition < _videoDuration) && !_controller.value.isPlaying){
+          _playAndPauseIcon = Icon(Icons.play_arrow_rounded);
+        }else if((_videoPosition == _videoDuration) && !_controller.value.isPlaying){
+          _playAndPauseIcon = Icon(Icons.replay_rounded);
+        }else if((_videoPosition == _videoDuration) && _controller.value.isPlaying){
+          _controller.seekTo(Duration(milliseconds: 0));
+        }
+
+        if(_getVolume > 0.5){
+          _volIcon = Icon(Icons.volume_up_rounded);
+        }else if(_getVolume <= 0.5 && _getVolume > 0){
+          _volIcon = Icon(Icons.volume_down_rounded);
+        }else if(_getVolume == 0){
+          _volIcon = Icon(Icons.volume_off_rounded);
+        }else{
+          _volIcon = Icon(Icons.error);
+        }
       });
     });
-    _controller.setLooping(false);
+
     _controller.initialize().then((value) => {
       setState(() {
         _hitTest = true;
@@ -48,13 +95,16 @@ class _videoPage extends State<videoPage>{
   @override
   void dispose() {
     _controller.dispose();
+    _volController.removeListener();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    _contextHeightPercent = MediaQuery.of(context).size.height / 100;
-    _contextWidthPercent = MediaQuery.of(context).size.width / 100;
+    _contextWidthPercent = MediaQuery.of(context).size.shortestSide / 100;
+    _contextHeightPercent = MediaQuery.of(context).size.longestSide / 100;
+
+    final double _statusBarHeight = MediaQuery.of(context).padding.top;
 
     return Scaffold(
       appBar: !_fullscreen ? AppBar(
@@ -84,11 +134,11 @@ class _videoPage extends State<videoPage>{
               Center(
                 child: _fullscreen ?
                 Container(
-                  child: _ControlsOverlay(controller: _controller, cntHPer: _contextHeightPercent, cntWPer: _contextWidthPercent),
+                  child: _ControlsOverlay(controller: _controller, volController: _volController, cntWPer: _contextWidthPercent, cntHPer: _contextHeightPercent, staHeight: _statusBarHeight+(3*_contextHeightPercent),),
                 ) :
                 AspectRatio(
                   aspectRatio: _controller.value.aspectRatio,
-                  child: _ControlsOverlay(controller: _controller, cntHPer: _contextHeightPercent, cntWPer: _contextWidthPercent),
+                  child: _ControlsOverlay(controller: _controller, volController: _volController, cntWPer: _contextWidthPercent, cntHPer: _contextHeightPercent,),
                 ),
               ),
 
@@ -101,11 +151,13 @@ class _videoPage extends State<videoPage>{
 }
 
 class _ControlsOverlay extends StatelessWidget {
-  const _ControlsOverlay({Key key, this.controller, this.cntHPer, this.cntWPer, this.fadeDur1 = 200, this.fadeDur2 = 5000}) : super(key: key);
+  const _ControlsOverlay({Key key, this.controller, this.volController, this.cntWPer, this.cntHPer, this.staHeight = 0, this.fadeDur1 = 200, this.fadeDur2 = 10000}) : super(key: key);
 
   final VideoPlayerController controller;
-  final double cntHPer;
+  final VolumeController volController;
   final double cntWPer;
+  final double cntHPer;
+  final double staHeight;
 
   final int fadeDur1;
   final int fadeDur2;
@@ -154,10 +206,8 @@ class _ControlsOverlay extends StatelessWidget {
               iconSize: 30*cntWPer,
               alignment: Alignment.center,
               padding: EdgeInsets.all(0),
-              icon: Icon(
-                controller.value.isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded,
-                color: Colors.white.withOpacity(0.8),
-              ),
+              color: Colors.white.withOpacity(0.8),
+              icon: _playAndPauseIcon,
               onPressed: () {
                 if(controller.value.isPlaying) {
                   controller.pause();
@@ -175,38 +225,108 @@ class _ControlsOverlay extends StatelessWidget {
             child: Stack(
               children: [
                 Align(
-                  alignment: Alignment.topRight,
-                  child: PopupMenuButton<double>(
-                    initialValue: controller.value.playbackSpeed,
-                    tooltip: 'Playback speed',
-                    onSelected: (speed) {
-                      _hitTest = true;
-                      autohide.auto();
-                      controller.setPlaybackSpeed(speed);
-                    },
-                    itemBuilder: (context) {
-                      return [
-                        for (final speed in _examplePlaybackRates)
-                          PopupMenuItem(
-                            value: speed,
-                            child: Text('${speed}x'),
-                          )
-                      ];
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 2*cntWPer,
-                        horizontal: 2*cntWPer,
-                      ),
-                      child: Text('${controller.value.playbackSpeed}x'),
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 4*cntHPer),
+                    child: IconButton(
+                      icon: Icon(Icons.fullscreen_rounded),
+                      color: Colors.white.withAlpha(90),
+                      iconSize: 6*cntWPer,
+                      alignment: Alignment.center,
+                      onPressed: () {
+                        if(_fullscreen){
+                          _fullscreen = false;
+                        }else{
+                          _fullscreen = true;
+                        }
+                        _hitTest = true;
+                        autohide.auto();
+                        controller.notifyListeners();
+                      },
                     ),
                   ),
-                ),
+                ), //풀스크린
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 4*cntHPer),
+                    child: IconButton(
+                      color: Colors.white.withAlpha(90),
+                      iconSize: 6*cntWPer,
+                      alignment: Alignment.center,
+                      icon: _playAndPauseIcon,
+                      onPressed: () {
+                        _hitTest = true;
+                        autohide.auto();
+                        if(controller.value.isPlaying) {
+                          controller.pause();
+                        }else{
+                          controller.play();
+                        }
+                      },
+                    ),
+                  ),
+                ), // 재생버튼
+                SizedBox(
+                  height: 6*cntHPer + staHeight,
+                  width: 30*cntWPer,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: staHeight,),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.zero,
+                            child: IconButton(
+                              color: Colors.white.withAlpha(90),
+                              iconSize: 6*cntWPer,
+                              alignment: Alignment.center,
+                              icon: _volIcon,
+                              onPressed: () {
+                                if(_getVolume > 0) {
+                                  _muteVolume = _getVolume;
+                                  volController.muteVolume();
+                                }else{
+                                  if(_muteVolume == 0){_muteVolume = 0.3;}
+                                  volController.setVolume(_muteVolume);
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 14*cntWPer, top: (6*cntHPer-1.5*cntWPer)/2,),
+                            child: SliderTheme(
+                              data: SliderThemeData(
+                                trackShape: CustomTrackShape(width: 26*cntWPer,height: 1.5*cntWPer,),
+                                thumbColor: Colors.red,
+                                activeTrackColor: Colors.red,
+                                inactiveTrackColor: Colors.white.withOpacity(0.5),
+                                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 1.5*cntWPer),
+                              ),
+                              child: Slider(
+                                value: _getVolume*100,
+                                min: 0,
+                                max: 100,
+                                onChanged: (value) {
+                                  volController.setVolume(value/100);
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),// 볼륨
                 Container(
                   alignment: Alignment.bottomCenter,
                   child: VideoProgressIndicator2(
                     controller,
-                    padding: EdgeInsets.only(left: 3*cntWPer, right: 3*cntWPer, bottom: 4*cntWPer),
+                    padding: EdgeInsets.only(left: 3*cntWPer, right: 3*cntWPer, bottom: 3*cntHPer),
                     colors: VideoProgressColors(
                       bufferedColor: Colors.white.withAlpha(80),
                       playedColor: Colors.red,
@@ -215,26 +335,7 @@ class _ControlsOverlay extends StatelessWidget {
                     height: 1*cntWPer,
                     radius: BorderRadius.all(Radius.circular(0.5*cntWPer)),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: IconButton(
-                    icon: Icon(Icons.fullscreen_rounded),
-                    color: Colors.white.withAlpha(80),
-                    iconSize: 6*cntWPer,
-                    padding: EdgeInsets.only(left: 3*cntWPer, right: 3*cntWPer, bottom: 8*cntWPer),
-                    onPressed: () {
-                      if(_fullscreen){
-                        _fullscreen = false;
-                      }else{
-                        _fullscreen = true;
-                      }
-                      _hitTest = true;
-                      autohide.auto();
-                      controller.notifyListeners();
-                    },
-                  ),
-                ),
+                ), // 중앙 재생버튼
               ],
             ),
             onTapDown: (details) {
@@ -262,10 +363,6 @@ class _ControlsOverlay extends StatelessWidget {
 class autohide {
   autohide();
 
-  Timer _autoHideTimeout = Timer(Duration(milliseconds: 6000), (){
-    _hitTest = false;
-  });
-
   autohide.auto([int _duration = 6000]){
     _autoHideTimeout?.cancel();
     if(_hitTest && !_hitTestHold) {
@@ -283,5 +380,26 @@ class autohide {
   autohide.hide(){
     _autoHideTimeout?.cancel();
     _hitTest = false;
+  }
+}
+
+class CustomTrackShape extends RoundedRectSliderTrackShape {
+  const CustomTrackShape({Key key, this.width, this.height,});
+
+  final double width;
+  final double height;
+
+  Rect getPreferredRect({
+    @required RenderBox parentBox,
+    Offset offset = Offset.zero,
+    @required SliderThemeData sliderTheme,
+    bool isEnabled = false,
+    bool isDiscrete = false,
+  }) {
+    final double trackHeight = height;
+    final double trackLeft = offset.dx;
+    final double trackTop = offset.dy;
+    final double trackWidth = width;
+    return Rect.fromLTWH(trackLeft, trackTop, trackWidth, trackHeight);
   }
 }
